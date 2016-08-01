a80fb00afa01e41dd19b22d86a26fd80d2217231
asdemirci <ayseselcuk77@gmail.com>
2016-08-01 16:13:57 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,7 +16,47 @@ sudo cp testserver.crt testserver.key ca.crt dh2048.pem /etc/openvpn
 # sudo scp testserver.crt testserver.key ca.crt dh2048.pem ta.key username@ipadresi:/home/username
 # ssh username@ipadresi
 # sudo apt-get install openvpn
-# 
+# sudo cp * /etc/openvpn
+#  cd /etc/openvpn
+# more server.conf
+# port 1194
+# proto udp
+# dev tun
+# ca ca.crt
+# cert testserver.crt
+# key testserver.key
+# dh dh2048.pem
+  
+# server 10.8.0.0 255.255.255.0
+# ifconfig-pool-persist ipp.txt
+
+# push "redirect-gateway def1"
+# push "dhcp-option DNS 8.8.8.8"
+# keepalive 10 120
+# comp-lzo
+# max-clients 100
+# persist-key
+# persist-tun
+# status openvpn-status.log
+# verb 3
+  
+  log openvpn.log
+  comp.lzo
+  push "redirect-gateway def1 bypass-dhcp"
+
+#  push "route 10.6.11.1 255.255.255.0"
+  
+  push "dhcp option DOMAIN testserver.com"
+  push "dhcp-option DNS 10.6.11.4"
+  
+  keepalive 1 3
+  
+  log openvpn.log
+  comp.lzo
+  push "redirect-gateway def1 bypass-dhcp"
+
+
+
 
 sudo cd ..
 source vars 
