8e63962d5522e53e3db19ffa25c0b3592844b5e5
asdemirci <ayseselcuk77@gmail.com>
2016-08-01 23:11:44 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+
+#server makinesinde yapılacaklar
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install openvpn easy-rsa
@@ -34,116 +36,34 @@ sudo cd /usr/share/doc/openvpn/examples/sample-config-files
 sudo cp /server.conf.gz /etc/openvpn
 sudo gzip –d /etc/openvpn/server.conf.gz
 sudo cp /client.conf /etc/openvpn
+sudo cd /etc/openvpn
+sudo su
+nano /etc/openvpn/server.conf
+# server.conf dosyasına veriler girilir
+
+echo "1" > /proc/sys/net/ipv4/ip_forward 
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT iptables -A FORWARD -i tun0 -o eth0 -j ACCEPT
+iptables-save 
+exit 0
+
+sudo /etc/init.d/openvpn restart 
+sudo openvpn /etc/openvpn/server.conf 
+#client makinasında yapılacaklar
+
+sudo apt-get update
+sudo apt-get install openvpn
+sudo mkdir /etc/openvpn
+sudo cd /etc/openvpn
+sudo cp /usr/share/doc/openvpn/examples/sample-config-files/client.conf client.conf
+sudo tar -cvf /etc/openvpn/clientsertifika.tar 
+sudo nano /etc/openvpnclient.conf
+# client.conf dosyasına veriler girilir
+sudo /etc/init.d/openvpn restart 
+sudo openvpn /etc/openvpn/client.conf
 
 
 
-
-# sudo scp testserver.crt testserver.key ca.crt dh2048.pem ta.key username@ipadresi:/home/username
-# ssh username@ipadresi
-# sudo apt-get install openvpn
-# sudo cp * /etc/openvpn
-#  cd /etc/openvpn
-# more server.conf
-# port 1194
-# proto udp
-# dev tun
-# ca ca.crt
-# cert testserver.crt
-# key testserver.key
-# dh dh2048.pem
-  
-# server 10.8.0.0 255.255.255.0
-# ifconfig-pool-persist ipp.txt
-
-# push "redirect-gateway def1"
-# push "dhcp-option DNS 8.8.8.8"
-# keepalive 10 120
-# comp-lzo
-# max-clients 100
-# persist-key
-# persist-tun
-# status openvpn-status.log
-# verb 3
-# 
-# cat /etc/resolv.conf
-# nameserver ipadresi
-# sudo /etc/init.d/openvpn stop
-# sudo /etc/init.d/openvpn start
-# netstat -an | GREP 1194
-# exit
-# exit
-
-
-# source vars
-# ./build-key testclient
-# more client.conf
-# client
-# dev tun
-# proto udp
-# remote serveradı ya da ip adresi 1194
-# nobind
-# resolv-retry infinite
-# tls-client
-# ca ca.crt
-# cert testclient.crt
-# key testclient. key
-# ns-cert-type testserver
-# cipher BF-CBC
-# tls-cipher DHE-RSA-AES256-SHA
-# tls-remote testserver
-## tls-auth /opt/local/etc/openvpn/tls-auth.key 1
-#remote-cert-tls testserver
-# comp-lzo
-# persist-key
-# persist-tun
-# mute-replay-warnings
-# verb 3
-# redirect-gateway def1
-# mlock
-
-
-
-  
-  log openvpn.log
-  comp.lzo
-  push "redirect-gateway def1 bypass-dhcp"
-
-#  push "route 10.6.11.1 255.255.255.0"
-  
-  push "dhcp option DOMAIN testserver.com"
-  push "dhcp-option DNS 10.6.11.4"
-  
-  keepalive 1 3
-  
-  log openvpn.log
-  comp.lzo
-  push "redirect-gateway def1 bypass-dhcp"
-
-
-
-
-sudo cd ..
-source vars 
- 
-sudo cd ..
-sudo touch server.conf
-sudo vim server.conf
-  ca ca.crt
-  cert testserver.crt
-  key testserver.key
-  dh dh2048.pem
-  
-  server 10.8.0.0 255.255.255.0
-  push "route 10.6.11.1 255.255.255.0"
-  
-  push "dhcp option DOMAIN testserver.com"
-  push "dhcp-option DNS 10.6.11.4"
-  
-  keepalive 1 3
-  
-  log openvpn.log
-  comp.lzo
-  push "redirect-gateway def1 bypass-dhcp"
 
 
 
